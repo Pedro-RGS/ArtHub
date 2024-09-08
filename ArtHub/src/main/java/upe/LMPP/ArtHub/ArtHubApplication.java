a557b4ad0d37c5f758ee98f4c51ca516d578500b
Pedro Ricardo Gomes de Souza <135163399+Pedro-RGS@users.noreply.github.com>
2024-09-08 16:27:58 -0300

    inicialização do projeto

---
+++ ArtHub/src/main/java/upe/LMPP/ArtHub/ArtHubApplication.java
@@ -0,0 +1,13 @@
+package upe.LMPP.ArtHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtHubApplication.class, args);
+	}
+
+}
---
+++ ArtHub/src/test/java/upe/LMPP/ArtHub/ArtHubApplicationTests.java
@@ -0,0 +1,13 @@
+package upe.LMPP.ArtHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
