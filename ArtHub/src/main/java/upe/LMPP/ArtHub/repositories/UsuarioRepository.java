1b1c21e3e2ec6c5c4d6f35578bbca74480adea62
Murilo Lucena <muriloglucenacanal@gmail.com>
2024-10-05 22:48:32 -0300

    adicao de alguns recursos da implementacoes (ainda incompleto)

--- ArtHub/src/main/java/upe/LMPP/ArtHub/repositories/UsuarioRepository.java
+++ ArtHub/src/main/java/upe/LMPP/ArtHub/repositories/UsuarioRepository.java
@@ -3,5 +3,10 @@ package upe.LMPP.ArtHub.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import upe.LMPP.ArtHub.entities.Usuario;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    public Optional<Usuario> findByEmail(String email);
+    public Optional<Usuario> findByApelido(String apelido);
+
 }
--- ArtHub/src/main/java/upe/LMPP/ArtHub/services/implementations/UsuarioServiceImpl.java
+++ ArtHub/src/main/java/upe/LMPP/ArtHub/services/implementations/UsuarioServiceImpl.java
@@ -1,4 +1,71 @@
 package upe.LMPP.ArtHub.services.implementations;
 
-public class UsuarioServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import upe.LMPP.ArtHub.entities.Publicacao;
+import upe.LMPP.ArtHub.entities.Usuario;
+import upe.LMPP.ArtHub.repositories.UsuarioRepository;
+import upe.LMPP.ArtHub.services.interfaces.UsuarioService;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class UsuarioServiceImpl implements UsuarioService {
+
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    @Override
+    public Usuario cadastrarUsuario(Usuario usuario) {
+        Optional<Usuario> usuarioBanco = usuarioRepository.findByEmail(usuario.getEmail());
+
+        if (usuarioBanco.isPresent()) {
+            return null; //Aqui vai a Exception de quando já existir o usuário
+        }
+
+        return usuarioRepository.save(usuario);
+    }
+
+    @Override
+    public Usuario atualizarUsuario(Usuario usuario) {
+        Optional<Usuario> usuarioBanco = usuarioRepository.findByEmail(usuario.getEmail());
+
+        if (usuarioBanco.isPresent()){
+            return usuarioRepository.save(usuario);
+        }
+
+        return null; //Aqui vai a Exception de quando não existir usuário
+    }
+
+    @Override
+    public void removerUsuario(Usuario usuario) {
+
+    }
+
+    @Override
+    public Usuario buscarUsuarioPorId(Integer id) {
+        return usuarioRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Usuario buscarUsuarioPorApelido(String apelido) {
+        return usuarioRepository.findByApelido(apelido).orElse(null);
+    }
+
+    @Override
+    public List<Publicacao> buscarPublicacoesPorUsuario(Integer id) {
+        // Tirar duvida
+        return List.of();
+
+    }
+
+    @Override
+    public List<Publicacao> buscarPublicacoesCurtidasPorUsuario(Integer id) {
+        // Tirar duvida
+        return List.of();
+    }
 }
+
