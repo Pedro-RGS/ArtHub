8678532d4ed2611c4eaf8834eb56e558618b65aa
paulabeatrizz <paulinhabeatrizcontato@gmail.com>
2024-10-16 19:37:44 -0300

    service

--- ArtHub/src/main/java/upe/LMPP/ArtHub/services/implementations/PublicacaoServiceImpl.java
+++ ArtHub/src/main/java/upe/LMPP/ArtHub/services/implementations/PublicacaoServiceImpl.java
@@ -1,9 +1,10 @@
 package upe.LMPP.ArtHub.services.implementations;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import upe.LMPP.ArtHub.entities.Publicacao;
+import upe.LMPP.ArtHub.repositories.PublicacaoRepository;
+
 public class PublicacaoServiceImpl {
-
-
-
 
 
 
