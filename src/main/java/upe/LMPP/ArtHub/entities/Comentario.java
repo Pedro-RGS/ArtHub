4161f990899b24e932c8a773e73fa3629743de4c
Pedro-RGS <pedro.gsouza@upe.br>
2024-10-16 20:29:42 -0300

    Fix: Remoção de pastas e pacotes duplicados

--- src/main/java/upe/LMPP/ArtHub/entities/Comentario.java
+++ src/main/java/upe/LMPP/ArtHub/entities/Comentario.java
@@ -7,26 +7,27 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
+@Table(name="comentario")
 @NoArgsConstructor
 @AllArgsConstructor
 @Setter
 @Getter
 public class Comentario {
     @Id
-    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
-    @ManyToOne
-    @JoinColumn(name = "id_publicacao")
-    private Publicacao publicacao;
 
     @ManyToOne
     @JoinColumn(name = "id_usuario")
     private Usuario usuario;
 
+    @ManyToOne
+    @JoinColumn(name = "id_publicacao")
+    private Publicacao publicacao;
     private Integer curtidas;
-
     private LocalDateTime dataPublicacao;
+
 }
