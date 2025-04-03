69633ad7d48bd9c28ed7ce82ed6a52cafc603c84
Pedro-RGS <pedro.gsouza@upe.br>
2025-04-03 18:34:31 -0300

    chore: criação de um novo pacote chamado .configuration

--- src/main/java/upe/LMPP/ArtHub/configuration/corsConfig/CorsConfig.java
+++ src/main/java/upe/LMPP/ArtHub/configuration/corsConfig/CorsConfig.java
@@ -1,4 +1,4 @@
-package upe.LMPP.ArtHub.corsconfig;
+package upe.LMPP.ArtHub.configuration.corsConfig;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
