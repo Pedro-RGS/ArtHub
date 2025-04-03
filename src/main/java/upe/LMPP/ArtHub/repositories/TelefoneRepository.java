7db8c61fa2dcf75b360dca29552e7c04bc1a5f4b
Pedro-RGS <pedro.gsouza@upe.br>
2025-04-03 18:39:12 -0300

    refactor: Deleção da classe Telefone e do seu repository

--- src/main/java/upe/LMPP/ArtHub/repositories/TelefoneRepository.java
+++
@@ -1,8 +0,0 @@
-package upe.LMPP.ArtHub.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import upe.LMPP.ArtHub.entities.Telefone;
-
-public interface TelefoneRepository extends JpaRepository<Telefone, Integer> {
-
-}
