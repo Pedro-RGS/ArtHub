a0f2f4240e69d03b5ee8f084edf597b3e8fc01f7
Pedro-RGS <pedro.gsouza@upe.br>
2025-04-11 15:36:12 -0300

    cleanup

--- src/main/java/upe/LMPP/ArtHub/business/services/implementations/PublicacaoServiceImpl.java
+++ src/main/java/upe/LMPP/ArtHub/business/services/implementations/PublicacaoServiceImpl.java
@@ -2,6 +2,8 @@ package upe.LMPP.ArtHub.business.services.implementations;
 
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import upe.LMPP.ArtHub.business.services.interfaces.PerfilService;
