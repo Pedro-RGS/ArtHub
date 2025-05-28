3feccefea72992469a5429d0fa88ae46cc884c95
Pedro-RGS <pedro.gsouza@upe.br>
2025-05-28 07:59:54 -0300

    fix: PerfilDTO agora possui associação com UsuarioDTO e o nome do seu método foi alterado

--- src/main/java/upe/LMPP/ArtHub/controller/DTO/pefil/PerfilDTO.java
+++ src/main/java/upe/LMPP/ArtHub/controller/DTO/pefil/PerfilDTO.java
@@ -1,13 +1,21 @@
 package upe.LMPP.ArtHub.controller.DTO.pefil;
 
+import upe.LMPP.ArtHub.controller.DTO.usuario.UsuarioDTO;
 import upe.LMPP.ArtHub.infra.entities.Perfil;
 
 public record PerfilDTO(
         String biografia,
         String fotoPerfil,
-        String banner) {
+        String banner,
+        UsuarioDTO usuario
+) {
 
-    public static PerfilDTO toPerfilDTO (Perfil perfil){
-        return new PerfilDTO(perfil.getBiografia(), perfil.getFotoPerfil(), perfil.getBiografia());
+    public static PerfilDTO perfilToDTO(Perfil perfil){
+        return new PerfilDTO(
+                perfil.getBiografia(),
+                perfil.getFotoPerfil(),
+                perfil.getBiografia(),
+                UsuarioDTO.UsuarioToDTO(perfil.getUsuario())
+        );
     }
 }
