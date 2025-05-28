0087e5d7e034a4e0224a9d7fb0a200af639a3d28
Pedro-RGS <pedro.gsouza@upe.br>
2025-05-28 08:03:30 -0300

    clear

--- src/main/java/upe/LMPP/ArtHub/controller/DTO/usuario/UsuarioCriadoDTO.java
+++ src/main/java/upe/LMPP/ArtHub/controller/DTO/usuario/UsuarioCriadoDTO.java
@@ -2,7 +2,6 @@ package upe.LMPP.ArtHub.controller.DTO.usuario;
 
 import java.util.Date;
 
-// Criar um UsuarioCreationDTO
 public record UsuarioCriadoDTO(
         String nome,
         String apelido,
