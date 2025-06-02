54d6fd845111dae9eaef77336919479eea258212
Pedro-RGS <pedro.gsouza@upe.br>
2025-06-02 14:49:39 -0300

    feat: adicionada a possibilidade de enviar arquivos de outros tipos além da imagem

--- src/main/java/upe/LMPP/ArtHub/business/services/implementations/MediaService.java
+++ src/main/java/upe/LMPP/ArtHub/business/services/implementations/MediaService.java
@@ -1,12 +1,8 @@
 package upe.LMPP.ArtHub.business.services.implementations;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ByteArrayResource;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -15,26 +11,36 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @Service
-public class ImageService {
+public class MediaService {
 
     // metodo para determinar o tipo de extenção do arquivo
     public static MediaType getMediaType(String fileName){
-        String extension = fileName.substring(fileName.lastIndexOf(".")).toLowerCase();
+        String extension = "";
+        int dotIndex = fileName.lastIndexOf(".");
+        if (dotIndex > 0){
+            extension = fileName.substring(dotIndex).toLowerCase();
+        }
         return switch (extension) {
             case ".jpg", ".jpeg" -> MediaType.IMAGE_JPEG;
             case ".png" -> MediaType.IMAGE_PNG;
             case ".gif" -> MediaType.IMAGE_GIF;
+            case ".mp4" -> MediaType.valueOf("video/mp4");
+            case ".webm" -> MediaType.valueOf("video/webm");
+            case ".mp3" -> MediaType.valueOf("audio/mpeg");
+            case ".wav" -> MediaType.valueOf("audio/wav");
+            case ".ogg" -> MediaType.valueOf("audio/ogg");
+            case ".opus" -> MediaType.valueOf("audio/opus");
             default -> MediaType.APPLICATION_OCTET_STREAM;
         };
     }
 
     // metodo para pegar a imagem baseado no path dela
-    public ByteArrayResource getImage(String fileName, String filePath) throws IOException {
+    public ByteArrayResource getFile(String fileName, String filePath) throws IOException {
         Path path = Paths.get(filePath).resolve(fileName).normalize();
         if(!Files.exists(path)){
             throw new FileNotFoundException("O arquivo com o seguinte path não foi encontrado: " + fileName);
         }
-        byte[] imagem = Files.readAllBytes(path);
-        return new ByteArrayResource(imagem);
+        byte[] fileBytes = Files.readAllBytes(path);
+        return new ByteArrayResource(fileBytes);
     }
 }
